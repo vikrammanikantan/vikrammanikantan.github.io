961cccaeafd7416236dc621e3778ef455d012c81
vikrammanikantan <vik@arizona.edu>
2023-11-05 21:10:04 -0700

    landing page, navbar completed

---
+++ assets/js/navbar.js
@@ -0,0 +1,12 @@
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (window.scrollY > 400) {
+    document.getElementById("navbar").style.top = "0px";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+}
